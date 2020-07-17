38153210fdec3ede0f08200a948c71ab9279f097
Alex Henry <tukkek@gmail.com>
2020-07-17 16:59:56 -0300

    Adopted a new structure, much simpler, faster and generally better

--- router.js
+++ router.js
@@ -17,59 +17,36 @@ function round(level){
   return level<10?level:Math.round(level/10)*10
 }
 
-/* As is, Act 1 (normal) is always the first one, as 
- */
 class Route{
   constructor(){
-    this.acts=[]//array of [level,act,elite]
+    this.milestones={//TODO should be easy enough to have 1 milestone = 1 act, no act 1 while leveling!
+      1:['Aspirant','','Crucible'], //crucible
+      20:false, //normal - or elite from get-go?
+      30:false, 
+      40:false,
+      50:false, //elite
+      60:false,
+      70:['Ultimate','Act 1','Main quest'], //act 1 ultimate
+      100:['Ultimate','Act 7','Shattered Realm'],
+    }
   }
   
   get last(){return this.acts.length>0&&this.acts[this.acts.length-1][1]} //last act
   
-  /*get nextlevel(){
-    let last=this.acts[this.acts.length-1]
-    let min=last[0]+10
-    let max=last[1].levels[1]
-    let a=roll(min,max)
-    let b=roll(min,max)
-    return Math.min(a,b)
-  }*/
-  
-  //TODO try using levels from wiki instead, only act 1 and 7 being used for normal
+  //TODO can try using levels from wiki instead
   generate(){
-    let elite=false
-    for(let level=1;level<90;){
-      let candidates=acts.sequential
-      let last=this.last
-      if(last){
-        candidates=candidates.filter(c=>this.acts.map(a=>a[1]).indexOf(c)<0)
-        candidates=candidates.filter(
-          a=>a.index==-1||a.index>last.index||(!elite&&!a.isnormal(level)&&a.iselite(level)))
-      }
-      candidates=candidates.filter(a=>(!elite&&a.isnormal(level))||a.iselite(level))
-      if(candidates.length==0){
-        level+=1
-        continue
-      }
-      let act=pick(candidates)
-      if(act==last) return false
-      if(!elite) elite=!act.isnormal(level)&&act.iselite(level)
-      this.acts.push([level,act,elite])
-      let min=level+1
-      let max=Math.min(level+9,elite?act.elite[1]:act.normal[1])
-      level=roll(min,max)
-      if(!this.validate()) return false
+    let pool=new Set([2,3,4,5,6,7])
+    for(let level=20;level<=60;level+=10){
+      if(pool.size==0) return false
+      let difficulty=level<50?'Normal':'Elite'
+      let candidates=Array.from(pool).flatMap(act=>acts.acts[act])
+      let arealevel=level+roll(1,4)-1
+      candidates=candidates.filter(
+        area=>difficulty=='Normal'?area.isnormal(arealevel):area.iselite(arealevel))
+      let area=pick(Array.from(candidates))
+      this.milestones[level]=[difficulty,'Act '+area.act,area.name]
+      pool.delete(area.act)
     }
-    return this.acts.length<=9*2&&
-      new Set(this.acts.map(a=>a[1].act).filter(a=>2<=a&&a)).size>=6&&
-      true//this.acts.map(a=>a[1].act).filter(a=>2<=a&&a<=6).length>=3
-  }
-  
-  validate(){
-    for(let i=0;i<this.acts.length;i++)
-      for(let j=i+1;j<this.acts.length;j++)
-        if(this.acts[i][1]==this.acts[j][1])
-          return false
     return true
   }
 }
@@ -78,18 +55,14 @@ export function setup(){
   acts.setup()
   let r=false
   while(!r||!r.generate()) r=new Route()
-  for(let a of r.acts){
+  for(let m of Object.keys(r.milestones)){
     let level=document.createElement('div')
-    level.innerHTML='Level '+a[0]
+    level.innerHTML=m==100?'End-game':'Level '+m
     ROUTE.appendChild(level)
-    let difficulty=document.createElement('div')
-    difficulty.innerHTML=a[2]?'Elite':'Normal/veteran'
-    ROUTE.appendChild(difficulty)
-    let act=document.createElement('div')
-    if(a[1].act>=1) act.innerHTML=`Act ${a[1].act}`
-    ROUTE.appendChild(act)
-    let area=document.createElement('div')
-    area.innerHTML=a[1]
-    ROUTE.appendChild(area)
+    for(let column of r.milestones[m]){
+      let c=document.createElement('div')
+      c.innerHTML=column
+      ROUTE.appendChild(c)
+    }
   }
 }
